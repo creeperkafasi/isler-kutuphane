a9fd5f2a030b4bddf50c6f2f503f12fd2c8d5a51
creeperkafasi <creeperkafasi@users.noreply.github.com>
2024-03-29 10:26:29 +0000

    deploy: 954924cd0929189492963d5978ff0d1b6df55373

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,9 @@
 mkdir lists
 curl "http://www.isleronline.com/data.php?ver=2&key=L6RRENET" --output ./lists/matematik.xml
-curl "http://www.isleronline.com/data.php?ver=2&key=7SLAD2A9" --output ./lists/fizik.xml
-curl "http://www.isleronline.com/data.php?ver=2&key=GHPHH8DH" --output ./lists/kimya.xml
-curl "http://www.isleronline.com/data.php?ver=2&key=43PNCHDY" --output ./lists/biyoloji.xml
+curl "http://www.isleronline.com/data.php?ver=2&key=LLRNY3EP" --output ./lists/fizik.xml
+curl "http://www.isleronline.com/data.php?ver=2&key=K7E55GER" --output ./lists/kimya.xml
+curl "http://www.isleronline.com/data.php?ver=2&key=7THTHFCG" --output ./lists/biyoloji.xml
 curl "http://www.isleronline.com/data.php?ver=2&key=D6BACHBY" --output ./lists/tarih.xml
 curl "http://www.isleronline.com/data.php?ver=2&key=CLF145AE" --output ./lists/cografya.xml
 curl "http://www.isleronline.com/data.php?ver=2&key=7BKH1TE1" --output ./lists/felsefe.xml
-curl "http://www.isleronline.com/data.php?ver=2&key=CHMLGYBD" --output ./lists/edebiyat.xml
+curl "http://www.isleronline.com/data.php?ver=2&key=DDP32AD1" --output ./lists/edebiyat.xml
